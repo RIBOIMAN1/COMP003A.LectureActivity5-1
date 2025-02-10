0063aeab8b384348c1d529a03361da34829ada98
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-10 01:29:21 -0800

    Added code from step 4 of the 5.4 lecture activity.

--- COMP003A.LectureActivity5-1/Program.cs
+++ COMP003A.LectureActivity5-1/Program.cs
@@ -8,6 +8,19 @@
 		public int Year;
 
 		/// <summary>
+		/// Default constructor
+		/// </summary>
+		/// <param name="make"></param>
+		/// <param name="model"></param>
+		/// <param name="year"></param>
+		public Car(string make, string model, int year)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+		}
+
+		/// <summary>
 		/// Display the car information
 		/// </summary>
 		public void DisplayInfo()
@@ -18,12 +31,8 @@
 		{
 			static void Main(string[] args)
 			{
-				Car myCar = new Car(); // Create a new Car object
-				myCar.Make = "Toyota"; // Set the car's make
-				myCar.Model = "Supra"; // Set the car's model
-				myCar.Year = 2024; // Set the car's year
-
-				myCar.DisplayInfo(); // Display the car's information
+				Car myCar = new Car("Honda", "Civic", 2021); // Create a new Car object using the constructor
+				myCar.DisplayInfo(); // Display the car information
 			}
 		}
 	}
